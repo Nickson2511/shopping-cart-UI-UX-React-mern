a5416dd60aadbf38f9107cb246714bce984ff7d1
Nickson2511 <okwembanickson8@gmail.com>
2023-05-03 11:07:13 +0300

    Update Copyright.js

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -6,18 +6,19 @@ export default class Copyright extends Component {
         <h4>IMPORTANT LINKS</h4>
         <ul>
           <li>
-            <a href="https://github.com/basir/ecommerce-shopping-cart">
+            <a href=" https://github.com/Nickson2511/shopping-cart-UI-UX-React-mern.git">
               Download Source code{" "}
             </a>
           </li>
           <li>
             <a href="https://www.udemy.com/course/build-ecommerce-website-by-react-redux-in-one-hour/">
-              Watch Learning Video
+              Watch Learning Video which encouraged me to build the above application
             </a>
           </li>{" "}
           <li>
-            <a href="https://codingwithbasir.com/course/learn-react-redux/">
-              Learn React From Scratch
+                <p>If you may wish to check my current repositories from , consider followig the below link</p>
+            <a href="https://github.com/Nickson2511?tab=repositories">
+              Learn React From reading and updating my code to implement whatever you may wish and build something useful
             </a>
           </li>
         </ul>
